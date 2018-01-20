287e82502732aeb866d0eda5323e67c6b9a749f3
Xavier Ros Roca <xavier.ros.roca@estudiant.upc.edu>
2018-01-20 14:55:17 +0100

    Changing NA interpolation

--- 01_Scripts/merge.R
+++ 01_Scripts/merge.R
@@ -11,10 +11,10 @@ dt_cal[,date:=as.POSIXct(date)]
 dt_cal <- dt_cal[, c("station", "date", "FC_today", "FC_yesterday")]
 
 dt <- merge(dt_obs, dt_cal, by.x = c("id_station", "date"), by.y= c("station", "date"), all=T)
-dt=dt[order(date)]
-dt[date<"2015-01-01 00:00:00", no2_2:=na.interpolation(no2)]
-dt[date<"2015-01-01 00:00:00", FC_T_2:=na.interpolation(FC_today)]
-dt[date<"2015-01-01 00:00:00", FC_Y_2:=na.interpolation(FC_yesterday)]
+dt=dt[order(id_station, date)]
+dt[date<"2015-01-01 00:00:00", no2_2:=na.interpolation(no2, option = "spline")]
+dt[date<"2015-01-01 00:00:00", FC_T_2:=na.interpolation(FC_today, option = "spline")]
+dt[date<"2015-01-01 00:00:00", FC_Y_2:=na.interpolation(FC_yesterday, option = "spline")]
 dt <- dt[!is.na(height)]
 
 write.table(dt, "00_Dataset/dataset.csv", row.names = FALSE, sep=";")

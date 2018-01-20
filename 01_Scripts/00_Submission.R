b22cefd878adbbe6509f90add089889ddc0aeac5
Ana <anna.valdivia.garcia@gmail.com>
2018-01-20 11:40:36 +0100

    Final Subimission script.
    Select columns 'date' and 'station'.
    
    Probabilities are defined as 'prob'.

--- 01_Scripts/00_Submission.R
+++ 01_Scripts/00_Submission.R
@@ -1,2 +1,19 @@
-## RScript per la submission
+###################################
+#
+#         # FINAL SUBMISSION
+#
+###################################
 
+# Read csv
+path <- "~/Datathon_2018/00_Dataset/"
+dataName <- "data.csv"
+finalData <- read.csv(paste0(path, dataName))
+
+
+# Order columns
+finalData <- finalData[order(finalData$date, finalData$station),]
+
+# Seect probability
+finalSubmission <- finalData$prob
+
+write.csv(finalSubmission, "submission.csv", row.names = FALSE)

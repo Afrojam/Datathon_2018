6aa17657e5e413819691c4fecb3b7087adbce542
mporcar <josepmporcar@gmail.com>
2018-01-20 11:18:51 +0100

    reupload read obs

--- 01_Scripts/read_obs.R
+++ 01_Scripts/read_obs.R
@@ -1,13 +1,11 @@
-# Miquel
+# Read Obs
 setwd("~/Desktop/Datathon_2018")
-setwd("~/Desktop/datathon")
 library(data.table)
 data=data.table(id_station=character(), no2=numeric(), fecha=character())
 for(year in 2013:2015){
   listfiles<-list.files(path = paste0("obs/",year), pattern = ".csv")
   dt<-data.table()
   for(i in listfiles){
-    stat=strsplit(i,"\\_|\\.")[1]
     dt_aux=fread(paste0("obs/",year,"/",i))
     dt_aux=dt_aux[,c(4,12,14),with=FALSE]
     colnames(dt_aux)<-c("id_station","no2","fecha")
